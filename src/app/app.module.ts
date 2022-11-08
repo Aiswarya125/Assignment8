b6b2e7375e61d82d1776f18c81f006a758e27e73
Aiswarya125 <aiswarya.j@gadgeon.com>
2022-11-08 17:26:42 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ReversePipe } from './reverse.pipe';
+import { FilterPipe } from './filter.pipe';
+import { ShortenPipe } from './shorten.pipe';
+import { SortPipe } from './sort.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReversePipe,
+    FilterPipe,
+    ShortenPipe,
+    SortPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
